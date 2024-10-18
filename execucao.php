caea3aaf9a6bd81b42b9d2dabcf998c51040d096
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-10-18 02:24:20 -0300

    Update execucao.php

--- execucao.php
+++ execucao.php
@@ -11,19 +11,17 @@ $objeto = null;
 if ($tipo == 1) 
 {
     $objeto = new Residencial();
-    $objeto->setConsumo(readline("Informe o consumo (em Kwh): "));
 }elseif ($tipo == 2) 
 {
     $objeto = new Comercial();
-    $objeto->setConsumo(readline("Informe o consumo (em Kwh): ")); 
 }elseif ($tipo == 3) 
 {
     $objeto = new Industrial();
-    $objeto->setConsumo(readline("Informe o consumo (em Kwh): "));
 }else
 {
     print "Opção inválida!";
     die;
 }
 
+$objeto->setConsumo(readline("Informe o consumo (em Kwh): "));
 print "O valor a ser pago na fatura é de: R$" . $objeto->getValorFatura();
